86c4f1ed9e8e1e7029701a0efad1dd2f3d94fb64
Andrejpopof <andrejpopov.1430@gmail.com>
2020-01-20 03:59:19 +0100

    Dialog so komentar (NE SE VNESUVA TEKSTOT)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
